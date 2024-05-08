f004815f8f6eb4815ee7fd402149a291a62982f2
Fredrik Welander <welandfr@arcada.fi>
2024-05-08 14:45:21 +0300

    add logging

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import os
 from flask import Flask, request
 from dotenv import load_dotenv
-
 load_dotenv()
 
 PORT = os.environ.get('PORT', 8080)
 
 app = Flask(__name__)
 
+with app.app_context():
+    # Init code here
+    print(f'Flask app running on port {PORT}')
+
 @app.route("/")
 def index():
-    ret = {'msg': 'Rahti Flask demo works.', 'env_var': os.environ.get('ENV_VAR') }
+    print(f'{request.method} request from {request.remote_addr}')
+    ret = {
+        'msg': 'Rahti Flask demo works.', 
+        'env': os.environ.get('ENV_VAR', 'not working'), 
+        'method': request.method
+    }
 
     return ret
-
 
 @app.route("/test-route", methods = [ 'GET', 'POST' ])
 def test_route():
@@ -33,5 +40,5 @@ def test_route():
     return ret
 
 if __name__ == "__main__":
-    app.run(debug=True, port=PORT, host='0.0.0.0')
-    print(f'Flask app running on port {PORT}')
+    app.run(debug=True, port=PORT, host='0.0.0.0' )
+    
